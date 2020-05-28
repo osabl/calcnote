9561b4c1e53637e7dac7753ffd9897639ef332e6
osabl <andrii.osabliuk@gmail.com>
2020-05-28 20:48:22 +0300

    move to new file

--- src/js/main.js
+++ src/js/main.js
@@ -1,97 +1,14 @@
 import Parser from './parser'
 
-const textarea = document.querySelector('#content')
-const content = new Parser()
+const input = document.querySelector('#content')
+const output = document.querySelector('#result')
+const parser = new Parser()
 
-textarea.addEventListener('change', () => {
-    const strArray = content.getStringsArray(textarea)
-    console.clear()
+input.addEventListener('input', () => {
+    output.value = ''
+    const result = parser.parse(input.value)
 
-    for (const str of strArray) {
-        // console.log('NUMBERS: ' + content.parseNumbers(str))
-        // console.log('OPERATORS: ' + content.parseOperators(str))
-        // console.log('BRAKETS: ' + content.parseBrakets(str))
-        // console.log('VARIABLE: ' + content.parseVariables(str))
-        // console.log('TOKENS: ' + content.parseTokens(str))
-        console.log('RESULT: ' + calc(content.parseTokens(str)))
+    for (const value of result) {
+        output.value += value + '\n'
     }
 })
-
-function calc(tokens) {
-    const stackNum = []
-    const stackOther = []
-    const priorities = {
-        '+': 1,
-        '-': 1,
-        '/': 2,
-        '*': 2,
-    }
-    const regexp = {
-        number: /\d+/,
-        braket: /\(|\)/,
-        operator: /\+|\-|\*|\//
-    }
-
-    for (const token of tokens) {
-        if (regexp.number.test(token)) {
-            stackNum.push(token)
-        } else if (regexp.braket.test(token)) {
-            if (token === '(') {
-                stackOther.push(token)
-            } else if (token === ')') {
-                if (lookAtTop(stackOther) === '(') {
-                    stackOther.pop()
-                } else {
-                    while (lookAtTop(stackOther) !== '(') {
-                        stackNum.push( calculate( stackNum.pop(), stackNum.pop(), stackOther.pop() ) )
-                    }
-                    if (lookAtTop(stackOther) === '(') {
-                        stackOther.pop()
-                    }
-                }
-            }
-        } else if (regexp.operator.test(token)) {
-            while (lookAtTop(stackOther) !== '(' 
-            && priorities[lookAtTop(stackOther)] >= priorities[token]) {
-                stackNum.push( calculate( stackNum.pop(), stackNum.pop(), stackOther.pop() ) )
-            }
-
-            stackOther.push(token)
-        }
-    }
-
-    console.log('NUM: ' + stackNum, 'OTHER: ' + stackOther)
-
-    while (stackOther.length > 0
-        && stackNum.length - 1 === stackOther.length) {
-            stackNum.push( calculate( stackNum.pop(), stackNum.pop(), stackOther.pop() ) )
-    }
-    console.log('NUM: ' + stackNum, 'OTHER: ' + stackOther)
-
-    if (stackNum.length === 1) {
-        return stackNum[0]
-    } else {
-        return ''
-    }
-}
-
-function calculate(operandB, operandA, operation) {
-    switch (operation) {
-        case '+':
-            return Number(operandA) + Number(operandB)
-        case '-':
-            return Number(operandA) - Number(operandB)
-        case '/':
-            return Number(operandA) / Number(operandB)
-        case '*':
-            return Number(operandA) * Number(operandB)
-    }
-}
-
-function lookAtTop(arr) {
-    if (arr.length > 0) {
-        return arr[arr.length - 1]
-    } else {
-        return null
-    }
-}
--- src/js/parser.js
+++ src/js/parser.js
@@ -1,26 +1,109 @@
 export default class Parser {
-
-    getStringsArray(textarea) {
-        return textarea.value.split(/\n/)
+    constructor() {
+        this.number = '(?<=(^|\\+|\\-|\\*|\\/|\\^|\\()\\s*|\\s+)\\-*\\d+\\.*\\d*'
+        this.operator = '(?<!^)(\\+|\\-|\\*|\\/|\\^)(?<!$)'
+        this.braket = '\\(|\\)'
+        this.tokens = new RegExp(`${this.number}|${this.operator}|${this.braket}`, 'g')
+        this.variables = new RegExp('(?<=\W\$)\d+', 'g')
     }
 
-    parseTokens(string) {
-        return string.match(/(?<=(^|\s|\+|\-|\*|\/|\())\d+|(?<!^)(\+|\-|\*|\/)(?<!$)|\(|\)/g)
+    parse(fullString) {
+        const result = []
+        const strArray = fullString.split(/\n/)
+        console.log(strArray)
+
+        for (const str of strArray) {
+            if (str.length === 0) {
+                result.push('')
+            } else {
+                str.replace(this.variables, (match) => {
+                    if (result.length >= match) {
+                        return result[match - 1]
+                    } else {
+                        return ''
+                    }
+                })
+
+                const tokens = str.match(this.tokens)
+                result.push(this.calc(tokens))
+            }
+        }
+
+        return result
     }
 
-    parseNumbers(string) {
-        return string.match(/(?<=(^|\s|\+|\-|\*|\/|\())\d+/g)
+    calculate(operandB, operandA, operation) {
+        switch (operation) {
+            case '+':
+                return Number(operandA) + Number(operandB)
+            case '-':
+                return Number(operandA) - Number(operandB)
+            case '/':
+                return Number(operandA) / Number(operandB)
+            case '*':
+                return Number(operandA) * Number(operandB)
+            case '^':
+                return Math.pow(Number(operandA), Number(operandB))
+        }
     }
 
-    parseOperators(string) {
-        return string.match(/(?<!^)(\+|\-|\*|\/)(?<!$)/g)
-    }
+    calc(tokens) {
+        if (tokens === null) {
+            return ''
+        }
+        
+        const stackNum = []
+        const stackOther = []
+        const priorities = {
+            '+': 1,
+            '-': 1,
+            '/': 2,
+            '*': 2,
+            '^': 3
+        }
+        const regexp = {
+            number: /\d+\.*\d*/,
+            braket: /\(|\)/,
+            operator: /\+|\-|\*|\/|\^/
+        }
 
-    parseBrakets(string) {
-        return string.match(/\(|\)/g)
-    }
+        for (const token of tokens) {
+            if (regexp.number.test(token)) {
+                stackNum.push(token)
+            } else if (regexp.braket.test(token)) {
+                if (token === '(') {
+                    stackOther.push(token)
+                } else if (token === ')') {
+                    if (stackOther[stackOther.length - 1] === '(') {
+                        stackOther.pop()
+                    } else {
+                        while (stackOther[stackOther.length - 1] !== '(' && stackOther.length > 0) {
+                            stackNum.push(this.calculate(stackNum.pop(), stackNum.pop(), stackOther.pop()))
+                        }
+                        if (stackOther[stackOther.length - 1] === '(') {
+                            stackOther.pop()
+                        }
+                    }
+                }
+            } else if (regexp.operator.test(token)) {
+                while (stackOther[stackOther.length - 1] !== '(' &&
+                    priorities[stackOther[stackOther.length - 1]] >= priorities[token]) {
+                    stackNum.push(this.calculate(stackNum.pop(), stackNum.pop(), stackOther.pop()))
+                }
 
-    parseVariables(string) {
-        return string.match(/(?<=(^\$|\s\$|\(\$))\d+/g)
+                stackOther.push(token)
+            }
+        }
+
+        while (stackOther.length > 0 &&
+            stackNum.length - 1 === stackOther.length) {
+            stackNum.push(this.calculate(stackNum.pop(), stackNum.pop(), stackOther.pop()))
+        }
+
+        if (stackNum.length === 1 && !isNaN(stackNum[0])) {
+            return stackNum[0]
+        } else {
+            return ''
+        }
     }
 }
